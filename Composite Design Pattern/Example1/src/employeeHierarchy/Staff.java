3210109bfa07cfb2f9194d1b67e787af041f91d6
ParthoShuvo <shuvojitsahashuvo@gmail.com>
2016-03-02 13:06:09 +0600

    Composite design pattern has been added

---
+++ Composite Design Pattern/Example1/src/employeeHierarchy/Staff.java
@@ -0,0 +1,10 @@
+package employeeHierarchy;
+
+public class Staff extends Employee{
+
+	public Staff(String name, String department, int salary) {
+		super(name, department, salary);
+		// TODO Auto-generated constructor stub
+	}
+
+}
